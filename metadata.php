ec4123e926b25b9ee4c77bb85656be5b313ea719
Ilja Weber <ilja.weber@mobilemojo.de>
2020-01-31 09:43:29 +0000

    release 3.3.0

--- metadata.php
+++ metadata.php
@@ -23,7 +23,7 @@ $aModule = array(
         'de' => 'Adressvalidierung + Korrekturvorschläge für Webshops',
     ),
     'thumbnail'    => 'endereco.png',
-    'version'      => '3.2.6',
+    'version'      => '3.3.0',
     'author'       => 'endereco',
     'email'        => 'info@endereco.de',
     'url'          => 'www.endereco.de',
